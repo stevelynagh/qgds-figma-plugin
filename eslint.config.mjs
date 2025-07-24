56e5b680d05aef675fb88bb5aca3c867c25a57d5
stevelynagh <steve.lynagh@smartservice.qld.gov.au>
2025-07-24 15:22:24 +1000

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,4 +3,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended);
+export default tseslint.config({
+  files: ["**/*.ts", "**/*.tsx"],
+  extends: [eslint.configs.recommended, tseslint.configs.recommended],
+});
