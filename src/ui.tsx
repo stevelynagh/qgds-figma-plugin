db5b82db2984833d272a584169c556c0b817aac2
stevelynagh <steve.lynagh@smartservice.qld.gov.au>
2025-07-24 11:10:20 +1000

    initial commit

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,55 +1,53 @@
-import '!prismjs/themes/prism.css'
+import "!prismjs/themes/prism.css";
 
-import {
-  Button,
-  Container,
-  render,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h, RefObject } from 'preact'
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
+import { Button, Container, render, VerticalSpace } from "@create-figma-plugin/ui";
+import { emit } from "@create-figma-plugin/utilities";
+import { h, RefObject } from "preact";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
+import { highlight, languages } from "prismjs";
+import Editor from "react-simple-code-editor";
 
-import styles from './styles.css'
-import { InsertCodeHandler } from './types'
+import styles from "./styles.css";
+import { InsertCodeHandler } from "./types";
 
 function Plugin() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
-  const containerElementRef : RefObject<HTMLDivElement> = useRef(null)
+  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
+  const containerElementRef: RefObject<HTMLDivElement> = useRef(null);
   const handleInsertCodeButtonClick = useCallback(
     function () {
-      emit<InsertCodeHandler>('INSERT_CODE', code)
+      emit<InsertCodeHandler>("INSERT_CODE", code);
     },
     [code]
-  )
+  );
   // Patch to make `react-simple-code-editor` compatible with Preact
-  useEffect(function () {
-    const containerElement = containerElementRef.current
-    if (containerElement === null) {
-      return
-    }
-    const textAreaElement = containerElement.querySelector('textarea')
-    if (textAreaElement === null) {
-      return
-    }
-    textAreaElement.textContent = code
-    const preElement = containerElement.querySelector('pre')
-    if (preElement === null) {
-      return
-    }
-    if (textAreaElement.nextElementSibling !== preElement) {
-      textAreaElement.after(preElement)
-    }
-  }, [code])
+  useEffect(
+    function () {
+      const containerElement = containerElementRef.current;
+      if (containerElement === null) {
+        return;
+      }
+      const textAreaElement = containerElement.querySelector("textarea");
+      if (textAreaElement === null) {
+        return;
+      }
+      textAreaElement.textContent = code;
+      const preElement = containerElement.querySelector("pre");
+      if (preElement === null) {
+        return;
+      }
+      if (textAreaElement.nextElementSibling !== preElement) {
+        textAreaElement.after(preElement);
+      }
+    },
+    [code]
+  );
   return (
     <Container space="medium">
       <VerticalSpace space="small" />
       <div class={styles.container} ref={containerElementRef}>
         <Editor
           highlight={function (code: string) {
-            return highlight(code, languages.js, 'js')
+            return highlight(code, languages.js, "js");
           }}
           onValueChange={setCode}
           preClassName={styles.editor}
@@ -63,7 +61,7 @@ function Plugin() {
       </Button>
       <VerticalSpace space="small" />
     </Container>
-  )
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
