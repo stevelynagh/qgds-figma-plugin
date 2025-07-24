2cb097acdcaa735312626a269cc348132533d3bb
stevelynagh <steve.lynagh@smartservice.qld.gov.au>
2025-07-24 13:51:05 +1000

    clear up Install tseslint

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
-import { EventHandler } from '@create-figma-plugin/utilities'
+import { EventHandler } from "@create-figma-plugin/utilities";
 
 export interface InsertCodeHandler extends EventHandler {
-  name: 'INSERT_CODE'
-  handler: (code: string) => void
+  name: "INSERT_CODE";
+  handler: (code: string) => void;
+}
+
+export interface Magic extends EventHandler {
+  name: "MAGIC";
+  handler: () => void;
 }
--- src/ui.tsx
+++ src/ui.tsx
@@ -1,6 +1,6 @@
 import "!prismjs/themes/prism.css";
 
-import { Button, Container, render, VerticalSpace } from "@create-figma-plugin/ui";
+import { Button, Container, render, VerticalSpace, Text } from "@create-figma-plugin/ui";
 import { emit } from "@create-figma-plugin/utilities";
 import { h, RefObject } from "preact";
 import { useCallback, useEffect, useRef, useState } from "preact/hooks";
@@ -11,52 +11,16 @@ import styles from "./styles.css";
 import { InsertCodeHandler } from "./types";
 
 function Plugin() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`);
-  const containerElementRef: RefObject<HTMLDivElement> = useRef(null);
-  const handleInsertCodeButtonClick = useCallback(
-    function () {
-      emit<InsertCodeHandler>("INSERT_CODE", code);
-    },
-    [code]
-  );
-  // Patch to make `react-simple-code-editor` compatible with Preact
-  useEffect(
-    function () {
-      const containerElement = containerElementRef.current;
-      if (containerElement === null) {
-        return;
-      }
-      const textAreaElement = containerElement.querySelector("textarea");
-      if (textAreaElement === null) {
-        return;
-      }
-      textAreaElement.textContent = code;
-      const preElement = containerElement.querySelector("pre");
-      if (preElement === null) {
-        return;
-      }
-      if (textAreaElement.nextElementSibling !== preElement) {
-        textAreaElement.after(preElement);
-      }
-    },
-    [code]
-  );
+  function magic(): void {
+    // Do stuff
+  }
+
   return (
     <Container space="medium">
       <VerticalSpace space="small" />
-      <div class={styles.container} ref={containerElementRef}>
-        <Editor
-          highlight={function (code: string) {
-            return highlight(code, languages.js, "js");
-          }}
-          onValueChange={setCode}
-          preClassName={styles.editor}
-          textareaClassName={styles.editor}
-          value={code}
-        />
-      </div>
+      <Text>Ready for some action?</Text>
       <VerticalSpace space="large" />
-      <Button fullWidth onClick={handleInsertCodeButtonClick}>
+      <Button fullWidth onClick={magic}>
         Insert Code
       </Button>
       <VerticalSpace space="small" />
