77500336790e8eb8c76d325a563d8ed172a0d7c7
Steve Lynagh <steve.lynagh@smartservice.qld.gov.au>
2025-07-30 11:14:02 +1000

    configure eslint to allow h and Fragment imports from preact

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
 
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  files: ["**/*.ts", "**/*.tsx"],
+  files: ['**/*.ts', '**/*.tsx'],
   extends: [eslint.configs.recommended, tseslint.configs.recommended],
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^(h|Fragment)$',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+      },
+    ],
+  },
 });
