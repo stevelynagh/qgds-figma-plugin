51ff8841bc9e46198b5cf860a05ef06adb89f650
Steve Lynagh <steve.lynagh@smartservice.qld.gov.au>
2025-08-01 14:13:36 +1000

    Enable plugin to run in dev mode

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
-import {
-  loadFontsAsync,
-  once,
-  on,
-  emit,
-  showUI,
-} from '@create-figma-plugin/utilities';
+import { on, emit, showUI } from '@create-figma-plugin/utilities';
 
 import {
-  InsertCodeHandler,
   ProcessVariablesHandler,
   ProcessVariablesCompleteHandler,
 } from './types';
 import { exportToJSON } from './utils/exportToJSON';
 
 export default function () {
-  once<InsertCodeHandler>('INSERT_CODE', async function (code: string) {
-    const text = figma.createText();
-    await loadFontsAsync([text]);
-    text.characters = code;
-    figma.currentPage.selection = [text];
-    figma.viewport.scrollAndZoomIntoView([text]);
-    figma.closePlugin();
-  });
-
   on<ProcessVariablesHandler>('PROCESS_VARIABLES', async () => {
     console.clear();
     const files = await exportToJSON();
